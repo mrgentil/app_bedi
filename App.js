78bdd24154aef3d96974ed9c89fa6da1863d9ae0
mrgentil <tshitshob@gmail.com>
2021-02-11 20:35:47 +0100

    all

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
+import Tabs from "./navigation/tab";
+import {Home,Restaurant,OrderDelivery} from './screens'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown:false}} initialRouteName={"Home"}>
+                <Stack.Screen name="Home" component={Tabs} />
+                <Stack.Screen name="Restaurant" component={Restaurant} />
+                <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
